af59751826cc6bc611cfe8884d8f16676ceb59ef
aozoragh <satoshi.a9877@gmail.com>
2026-04-06 20:06:36 +1000

    feat: simple html file and configure the simple logic

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log('Hello from Electron 👋')
+const { app, BrowserWindow } = require('electron/main')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  // On macOS it's common to re-create a window in the app when the dock icon is clicked and there are no other windows open.
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
